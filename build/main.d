c69ce1c80b4c0442ef80e69ed4df86bdbf6f51e2
MCJack123 <jackbruienne@yahoo.com>
2016-02-22 15:15:20 -0500

    Initial Commit

---
+++ build/main.d
@@ -0,0 +1,169 @@
+main.o: /Users/CommonFiles/ASCIIJump/ASCIIJump3DS/source/main.cpp \
+ /Users/CommonFiles/ASCIIJump/ASCIIJump3DS/source/3DSTerm.hpp \
+ /opt/devkitPro/libctru/include/3ds.h \
+ /opt/devkitPro/libctru/include/3ds/types.h \
+ /opt/devkitPro/libctru/include/3ds/result.h \
+ /opt/devkitPro/libctru/include/3ds/ipc.h \
+ /opt/devkitPro/libctru/include/3ds/svc.h \
+ /opt/devkitPro/libctru/include/3ds/srv.h \
+ /opt/devkitPro/libctru/include/3ds/os.h \
+ /opt/devkitPro/libctru/include/3ds/synchronization.h \
+ /opt/devkitPro/libctru/include/3ds/thread.h \
+ /opt/devkitPro/libctru/include/3ds/gfx.h \
+ /opt/devkitPro/libctru/include/3ds/services/gspgpu.h \
+ /opt/devkitPro/libctru/include/3ds/console.h \
+ /opt/devkitPro/libctru/include/3ds/env.h \
+ /opt/devkitPro/libctru/include/3ds/util/utf.h \
+ /opt/devkitPro/libctru/include/3ds/allocator/linear.h \
+ /opt/devkitPro/libctru/include/3ds/allocator/mappable.h \
+ /opt/devkitPro/libctru/include/3ds/allocator/vram.h \
+ /opt/devkitPro/libctru/include/3ds/services/ac.h \
+ /opt/devkitPro/libctru/include/3ds/services/am.h \
+ /opt/devkitPro/libctru/include/3ds/services/apt.h \
+ /opt/devkitPro/libctru/include/3ds/services/cam.h \
+ /opt/devkitPro/libctru/include/3ds/services/y2r.h \
+ /opt/devkitPro/libctru/include/3ds/services/cfgnor.h \
+ /opt/devkitPro/libctru/include/3ds/services/cfgu.h \
+ /opt/devkitPro/libctru/include/3ds/services/csnd.h \
+ /opt/devkitPro/libctru/include/3ds/services/dsp.h \
+ /opt/devkitPro/libctru/include/3ds/services/fs.h \
+ /opt/devkitPro/libctru/include/3ds/services/gsplcd.h \
+ /opt/devkitPro/libctru/include/3ds/services/hid.h \
+ /opt/devkitPro/libctru/include/3ds/services/irrst.h \
+ /opt/devkitPro/libctru/include/3ds/services/httpc.h \
+ /opt/devkitPro/libctru/include/3ds/services/ir.h \
+ /opt/devkitPro/libctru/include/3ds/services/ns.h \
+ /opt/devkitPro/libctru/include/3ds/services/pm.h \
+ /opt/devkitPro/libctru/include/3ds/services/ps.h \
+ /opt/devkitPro/libctru/include/3ds/services/ptmu.h \
+ /opt/devkitPro/libctru/include/3ds/services/ptmsysm.h \
+ /opt/devkitPro/libctru/include/3ds/services/soc.h \
+ /opt/devkitPro/libctru/include/3ds/services/mic.h \
+ /opt/devkitPro/libctru/include/3ds/services/mvd.h \
+ /opt/devkitPro/libctru/include/3ds/services/news.h \
+ /opt/devkitPro/libctru/include/3ds/services/qtm.h \
+ /opt/devkitPro/libctru/include/3ds/services/srvpm.h \
+ /opt/devkitPro/libctru/include/3ds/services/hb.h \
+ /opt/devkitPro/libctru/include/3ds/gpu/gx.h \
+ /opt/devkitPro/libctru/include/3ds/gpu/gpu.h \
+ /opt/devkitPro/libctru/include/3ds/gpu/registers.h \
+ /opt/devkitPro/libctru/include/3ds/gpu/enums.h \
+ /opt/devkitPro/libctru/include/3ds/gpu/gpu-old.h \
+ /opt/devkitPro/libctru/include/3ds/gpu/shbin.h \
+ /opt/devkitPro/libctru/include/3ds/gpu/shaderProgram.h \
+ /opt/devkitPro/libctru/include/3ds/ndsp/ndsp.h \
+ /opt/devkitPro/libctru/include/3ds/ndsp/channel.h \
+ /opt/devkitPro/libctru/include/3ds/sdmc.h \
+ /opt/devkitPro/libctru/include/3ds/romfs.h
+
+/Users/CommonFiles/ASCIIJump/ASCIIJump3DS/source/3DSTerm.hpp:
+
+/opt/devkitPro/libctru/include/3ds.h:
+
+/opt/devkitPro/libctru/include/3ds/types.h:
+
+/opt/devkitPro/libctru/include/3ds/result.h:
+
+/opt/devkitPro/libctru/include/3ds/ipc.h:
+
+/opt/devkitPro/libctru/include/3ds/svc.h:
+
+/opt/devkitPro/libctru/include/3ds/srv.h:
+
+/opt/devkitPro/libctru/include/3ds/os.h:
+
+/opt/devkitPro/libctru/include/3ds/synchronization.h:
+
+/opt/devkitPro/libctru/include/3ds/thread.h:
+
+/opt/devkitPro/libctru/include/3ds/gfx.h:
+
+/opt/devkitPro/libctru/include/3ds/services/gspgpu.h:
+
+/opt/devkitPro/libctru/include/3ds/console.h:
+
+/opt/devkitPro/libctru/include/3ds/env.h:
+
+/opt/devkitPro/libctru/include/3ds/util/utf.h:
+
+/opt/devkitPro/libctru/include/3ds/allocator/linear.h:
+
+/opt/devkitPro/libctru/include/3ds/allocator/mappable.h:
+
+/opt/devkitPro/libctru/include/3ds/allocator/vram.h:
+
+/opt/devkitPro/libctru/include/3ds/services/ac.h:
+
+/opt/devkitPro/libctru/include/3ds/services/am.h:
+
+/opt/devkitPro/libctru/include/3ds/services/apt.h:
+
+/opt/devkitPro/libctru/include/3ds/services/cam.h:
+
+/opt/devkitPro/libctru/include/3ds/services/y2r.h:
+
+/opt/devkitPro/libctru/include/3ds/services/cfgnor.h:
+
+/opt/devkitPro/libctru/include/3ds/services/cfgu.h:
+
+/opt/devkitPro/libctru/include/3ds/services/csnd.h:
+
+/opt/devkitPro/libctru/include/3ds/services/dsp.h:
+
+/opt/devkitPro/libctru/include/3ds/services/fs.h:
+
+/opt/devkitPro/libctru/include/3ds/services/gsplcd.h:
+
+/opt/devkitPro/libctru/include/3ds/services/hid.h:
+
+/opt/devkitPro/libctru/include/3ds/services/irrst.h:
+
+/opt/devkitPro/libctru/include/3ds/services/httpc.h:
+
+/opt/devkitPro/libctru/include/3ds/services/ir.h:
+
+/opt/devkitPro/libctru/include/3ds/services/ns.h:
+
+/opt/devkitPro/libctru/include/3ds/services/pm.h:
+
+/opt/devkitPro/libctru/include/3ds/services/ps.h:
+
+/opt/devkitPro/libctru/include/3ds/services/ptmu.h:
+
+/opt/devkitPro/libctru/include/3ds/services/ptmsysm.h:
+
+/opt/devkitPro/libctru/include/3ds/services/soc.h:
+
+/opt/devkitPro/libctru/include/3ds/services/mic.h:
+
+/opt/devkitPro/libctru/include/3ds/services/mvd.h:
+
+/opt/devkitPro/libctru/include/3ds/services/news.h:
+
+/opt/devkitPro/libctru/include/3ds/services/qtm.h:
+
+/opt/devkitPro/libctru/include/3ds/services/srvpm.h:
+
+/opt/devkitPro/libctru/include/3ds/services/hb.h:
+
+/opt/devkitPro/libctru/include/3ds/gpu/gx.h:
+
+/opt/devkitPro/libctru/include/3ds/gpu/gpu.h:
+
+/opt/devkitPro/libctru/include/3ds/gpu/registers.h:
+
+/opt/devkitPro/libctru/include/3ds/gpu/enums.h:
+
+/opt/devkitPro/libctru/include/3ds/gpu/gpu-old.h:
+
+/opt/devkitPro/libctru/include/3ds/gpu/shbin.h:
+
+/opt/devkitPro/libctru/include/3ds/gpu/shaderProgram.h:
+
+/opt/devkitPro/libctru/include/3ds/ndsp/ndsp.h:
+
+/opt/devkitPro/libctru/include/3ds/ndsp/channel.h:
+
+/opt/devkitPro/libctru/include/3ds/sdmc.h:
+
+/opt/devkitPro/libctru/include/3ds/romfs.h:
