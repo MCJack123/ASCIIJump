1f60f7305e15252e980dba78e0fa2ca3d29a39a1
JackMacWindows <jackbruienne@yahoo.com>
2018-01-19 11:56:51 -0500

    Update util.h

--- source/util.h
+++ source/util.h
@@ -3,6 +3,18 @@
 
 typedef char *audio;
 
+enum keypress {
+    KEY_UP,
+    KEY_DOWN,
+    KEY_LEFT,
+    KEY_RIGHT,
+    KEY_SELECT,
+    KEY_JUMP,
+    KEY_BLOCKCHANGE,
+    KEY_LEVELEDIT,
+    KEY_EDITCHANGE
+}
+
 extern void initialize();
 extern void exit();
 extern void print(std::string text);
